75935b7279b2e229466c68be3c30a715628821c3
Georgii Voyakin <voyaking@gmail.com>
2022-10-17 16:42:33 +0300

    Update controller practice-04

--- practice-04/spring-app/src/main/kotlin/com/example/demo/MainController.kt
+++ practice-04/spring-app/src/main/kotlin/com/example/demo/MainController.kt
@@ -1,2 +1,13 @@
 package com.example.demo
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class MainController(
+    private val carRepository: CarRepository, 
+    private val clientRepository: ClientRepository, 
+    private val malfunctionRepository: MalfunctionRepository
+) {
+  
+}
