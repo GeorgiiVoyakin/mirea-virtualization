cbd237e45c93b5790046d3a6c8653953cc72a550
Georgii Voyakin <voyaking@gmail.com>
2022-10-17 10:46:01 +0300

    Update spring app

--- practice-03/spring-app/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ practice-03/spring-app/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,9 +2,8 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
