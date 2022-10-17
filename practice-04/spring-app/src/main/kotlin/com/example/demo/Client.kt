1f2257d3705e00473683e9772e84bb4141d4f3fa
Georgii Voyakin <voyaking@gmail.com>
2022-10-17 16:25:58 +0300

    Update practice-04

--- practice-04/spring-app/src/main/kotlin/com/example/demo/Client.kt
+++ practice-04/spring-app/src/main/kotlin/com/example/demo/Client.kt
@@ -1,1 +1,12 @@
 package com.example.demo
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "Clients")
+data class Client(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
+    val name: String,
+)
