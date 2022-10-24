2a3afc0283fa18261824d6df803a32f0843ae0ed
GeorgiiVoyakin <voyaking@gmail.com>
2022-10-24 15:15:45 +0300

    Fix naming

--- practice-04/spring-app/src/main/kotlin/com/example/demo/MainController.kt
+++ practice-04/spring-app/src/main/kotlin/com/example/demo/MainController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class MainController(
     private val carRepository: CarRepository, 
-    private val clientRepository: ClientRepository, 
+    private val clientsRepository: ClientsRepository,
     private val malfunctionRepository: MalfunctionRepository
 ) {
   
